7b3419d2d0fc8008f5ff886e1a235512e1090862
tranlynhathao <haotran04022005@gmail.com>
2024-11-02 20:12:27 +0700

    adjust comment pug

--- lua/plugins/spec/comment.lua
+++ lua/plugins/spec/comment.lua
@@ -14,8 +14,17 @@ return {
     end, { desc = "Comment toggle (block) in single line" })
 
     map("n", "<leader>/", function()
-      api.toggle.linewise.current()
-    end, { desc = "Comment toggle" })
+      local line = vim.api.nvim_get_current_line()
+      if vim.bo.filetype == "pug" then
+        if line:sub(1, 2) == "//" then
+          vim.api.nvim_set_current_line(line:sub(4))
+        else
+          vim.api.nvim_set_current_line("// " .. line)
+        end
+      else
+        api.toggle.linewise.current()
+      end
+    end, { desc = "Comment toggle for Pug files" })
 
     map(
       "x",
@@ -28,20 +37,23 @@ return {
   config = function(_, opts)
     local comment = require "Comment"
     local ts_addon = require "ts_context_commentstring.integrations.comment_nvim"
+
     opts.pre_hook = function(ctx)
       vim.api.nvim_echo({ { vim.inspect(ctx), "Normal" } }, false, {})
-      vim.api.nvim_echo({ { vim.inspect(require("Comment.ft").ctype), "Normal" } }, false, {})
       if vim.bo.filetype == "pug" then
         if ctx.ctype == require("Comment.ft").ctype.line then
           return "// %s"
-        else
+        elseif ctx.ctype == require("Comment.ft").ctype.block then
           return "//- %s"
+        else
+          return "// %s"
         end
       end
 
       local ts_pre_hook = ts_addon.create_pre_hook()
       return ts_pre_hook and ts_pre_hook(ctx) or ""
     end
+
     comment.setup(opts)
   end,
 }
