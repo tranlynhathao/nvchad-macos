4b5423903943fafc0809094b1084128749968fb2
tranlynhathao <haotran04022005@gmail.com>
2024-11-14 22:13:02 +0700

    update plugin: showkeys, endhints

--- lua/plugins/spec/lsp-endhints.lua
+++ lua/plugins/spec/lsp-endhints.lua
@@ -1,3 +1,4 @@
+---@type NvPluginSpec
 return {
   "chrisgrieser/nvim-lsp-endhints",
   event = "LspAttach",
@@ -9,7 +10,7 @@ return {
     debounce = 150,
     show_if_no_lsp = false,
   },
-  config = function(_, opts)
-    require("nvim-lsp-endhints").setup(opts)
-  end,
+  -- config = function(_, opts)
+  --   require("nvim-lsp-endhints").setup(opts)
+  -- end,
 }
