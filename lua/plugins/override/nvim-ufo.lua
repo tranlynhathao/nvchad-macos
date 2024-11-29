b9bf16f3c403365f68409ac9e653e95402c68026
tranlynhathao <haotran04022005@gmail.com>
2024-11-29 21:14:53 +0700

    add ufo plugin

---
+++ lua/plugins/override/nvim-ufo.lua
@@ -0,0 +1,24 @@
+---@type NvPluginSpec
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = { "kevinhwang91/promise-async" },
+  config = function()
+    -- Turn on code folding
+    vim.o.foldcolumn = "1"
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+
+    -- `zc`: Close a code block (fold the block).
+    -- `zo`: Open a code block (unfold the block).
+    -- `zM`: Close all code blocks (fold all).
+    -- `zR`: Open all code blocks (unfold all).
+
+    -- Get Ufo into folding of Neovim
+    require("ufo").setup {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { "lsp", "treesitter", "indent" }
+      end,
+    }
+  end,
+}
