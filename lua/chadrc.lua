d2046e372ab1c5b89ef7e2d0fabfebecd54f0c03
tranlynhathao <haotran04022005@gmail.com>
2025-02-28 09:22:09 +0700

    chore: add formatter

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -10,7 +10,7 @@ local themes_customs = require("gale.chadrc_aux").themes_customs
 M.base46 = {
   transparency = true,
   theme = "gruvbox", ---@diagnostic disable-line -- TODO: remove kanagawa
-  theme_toggle = { "solarized_dark", "solarized_dark" }, ---@diagnostic disable-line --TODO remove kanagawa
+  theme_toggle = { "gruvbox", "gruvbox" }, ---@diagnostic disable-line --TODO remove kanagawa
   integrations = {
     "blankline",
     "cmp",
