3463940b6b6dca7a4c307af7b32cc9e71c3cb585
tranlynhathao <haotran04022005@gmail.com>
2025-04-10 09:29:57 +0700

    fix: update path for pbcopy & pbpaste

--- lua/gale/init.lua
+++ lua/gale/init.lua
@@ -8,6 +8,19 @@ require "gale.aliases"
 -- require "gale.linux"
 require "gale.wezterm"
 require "gale.macos"
+
+local is_mac = vim.loop.os_uname().sysname == "Darwin"
+local is_linux = vim.loop.os_uname().sysname == "Linux"
+local is_windows = vim.loop.os_uname().sysname:find "Windows" ~= nil
+
+if is_mac then
+  require "gale.macos"
+elseif is_linux then
+  require "gale.linux"
+elseif is_windows then
+  require "gale.windows"
+end
+
 require "gale.vim"
 require "gale.filetypes"
 require "gale.material-ui"
