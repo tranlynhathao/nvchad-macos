ac90faff56a158530757ae6b5c070bbe3763fe18
tranlynhathao <haotran04022005@gmail.com>
2025-04-13 18:00:22 +0700

    feat: add encode

--- lua/gale/encoding.lua
+++ lua/gale/encoding.lua
@@ -4,7 +4,7 @@ local M = {}
 function M.setup()
   vim.opt.encoding = "utf-8"
   vim.opt.fileencoding = "utf-8"
-  vim.opt.fileencodings = { "utf-8" }
+  vim.opt.fileencodings = { "utf-8", "ucs-bom", "euc-jp", "cp932", "sjis", "iso-2022-jp", "latin1" }
 end
 
 return M
