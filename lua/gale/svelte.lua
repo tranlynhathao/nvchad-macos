5eeac1019d2bf962090ec5a1590a0f593bddf390
tranlynhathao <haotran04022005@gmail.com>
2025-06-15 08:31:24 +0700

    feat: add dependencies for lsp

--- lua/gale/svelte.lua
+++ lua/gale/svelte.lua
@@ -1,3 +1,4 @@
+-- ~/.config/nvim/lua/plugins/svelte.lua
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -5,12 +6,22 @@ return {
       vim.list_extend(opts.ensure_installed, { "svelte" })
     end,
   },
+
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        svelte = {},
-      },
-    },
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+
+      opts.servers.svelte = {
+        on_attach = function(client, bufnr)
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = { "*.js", "*.ts" },
+            callback = function(ctx)
+              client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+            end,
+          })
+        end,
+      }
+    end,
   },
 }
