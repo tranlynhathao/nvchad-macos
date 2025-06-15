c32a4b4db92472e0b48aa73ce524b723b4817694
tranlynhathao <haotran04022005@gmail.com>
2025-06-15 10:33:13 +0700

    feat: add keymaps gotag

--- lua/plugins/spec/gopher.lua
+++ lua/plugins/spec/gopher.lua
@@ -8,6 +8,20 @@ return {
     -- Keymaps cho struct tags
     vim.keymap.set("n", "<leader>gsj", "<cmd>GoTagAdd json<CR>", { desc = "Add json struct tags" })
     vim.keymap.set("n", "<leader>gsy", "<cmd>GoTagAdd yaml<CR>", { desc = "Add yaml struct tags" })
+    vim.keymap.set("n", "<leader>gss", "<cmd>GoTagAdd sql<CR>", { desc = "Add sql struct tags" })
+    vim.keymap.set("n", "<leader>gsv", "<cmd>GoTagAdd validate<CR>", { desc = "Add validate struct tags" })
+    vim.keymap.set(
+      "n",
+      "<leader>gst",
+      "<cmd>GoTagAdd json,yaml,sql,validate<CR>",
+      { desc = "Add all common struct tags" }
+    )
+
+    -- Keymaps for removing struct tags
+    vim.keymap.set("n", "<leader>grj", "<cmd>GoTagRm json<CR>", { desc = "Remove json struct tags" })
+    vim.keymap.set("n", "<leader>gry", "<cmd>GoTagRm yaml<CR>", { desc = "Remove yaml struct tags" })
+    vim.keymap.set("n", "<leader>grs", "<cmd>GoTagRm sql<CR>", { desc = "Remove sql struct tags" })
+    vim.keymap.set("n", "<leader>grv", "<cmd>GoTagRm validate<CR>", { desc = "Remove validate struct tags" })
   end,
   build = function()
     vim.cmd [[silent! GoInstallDeps]]
