ab4fb24657cd7f676bf64542f79e659a7147db08
tranlynhathao <haotran04022005@gmail.com>
2025-12-24 13:02:00 +0700

    new setup

--- lua/plugins/spec/blockchain.lua
+++ lua/plugins/spec/blockchain.lua
@@ -1,4 +1,22 @@
+---@type NvPluginSpec
 return {
-  "TovarishFin/vim-solidity",
-  ft = { "solidity" },
+  -- Solidity syntax highlighting (fallback)
+  {
+    "TovarishFin/vim-solidity",
+    ft = { "solidity" },
+  },
+  -- Enhanced Solidity support with treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "solidity" })
+    end,
+  },
+  -- Foundry.toml syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "toml" })
+    end,
+  },
 }
