9ee38a197d30cb8fd1e5042af1e96eccb37d7845
tranlynhathao <haotran04022005@gmail.com>
2026-02-25 18:15:40 +0700

    feat: update weekly

--- lua/plugins/spec/surround.lua
+++ lua/plugins/spec/surround.lua
@@ -5,15 +5,13 @@ return {
   event = "VeryLazy",
   opts = {},
   config = function()
-    require("nvim-surround").setup {
-      keymaps = {
-        normal = "ys", -- word
-        normal_cur = "yss", -- line
-        delete = "ds",
-        change = "cs",
-        visual = "<leader>sr",
-        visual_line = "gS",
-      },
-    }
+    require("nvim-surround").setup()
+
+    -- vim.keymap.set("n", "ys", "<Plug>(nvim-surround-normal)")
+    -- vim.keymap.set("n", "yss", "<Plug>(nvim-surround-normal-cur)")
+    -- vim.keymap.set("n", "ds", "<Plug>(nvim-surround-delete)")
+    -- vim.keymap.set("n", "cs", "<Plug>(nvim-surround-change)")
+    -- vim.keymap.set("x", "<leader>sr", "<Plug>(nvim-surround-visual)")
+    -- vim.keymap.set("x", "gS", "<Plug>(nvim-surround-visual-line)")
   end,
 }
