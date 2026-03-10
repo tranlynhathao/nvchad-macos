c6b946286ac127d348473cf41876583d7647eae0
tranlynhathao <haotran04022005@gmail.com>
2026-03-10 15:59:39 +0700

    feat: update surround logic

--- lua/plugins/spec/surround.lua
+++ lua/plugins/spec/surround.lua
@@ -7,11 +7,11 @@ return {
   config = function()
     require("nvim-surround").setup()
 
-    -- vim.keymap.set("n", "ys", "<Plug>(nvim-surround-normal)")
-    -- vim.keymap.set("n", "yss", "<Plug>(nvim-surround-normal-cur)")
-    -- vim.keymap.set("n", "ds", "<Plug>(nvim-surround-delete)")
-    -- vim.keymap.set("n", "cs", "<Plug>(nvim-surround-change)")
-    -- vim.keymap.set("x", "<leader>sr", "<Plug>(nvim-surround-visual)")
-    -- vim.keymap.set("x", "gS", "<Plug>(nvim-surround-visual-line)")
+    vim.keymap.set("n", "ys", "<Plug>(nvim-surround-normal)")
+    vim.keymap.set("n", "yss", "<Plug>(nvim-surround-normal-cur)")
+    vim.keymap.set("n", "ds", "<Plug>(nvim-surround-delete)")
+    vim.keymap.set("n", "cs", "<Plug>(nvim-surround-change)")
+    vim.keymap.set("x", "<leader>sr", "<Plug>(nvim-surround-visual)")
+    vim.keymap.set("x", "gS", "<Plug>(nvim-surround-visual-line)")
   end,
 }
