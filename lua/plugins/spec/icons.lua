105ba40566ffcebdcc50402cb33c7a22ba136464
tranlynhathao <haotran04022005@gmail.com>
2026-03-30 17:23:09 +0700

    optimize nvim

--- lua/plugins/spec/icons.lua
+++ lua/plugins/spec/icons.lua
@@ -8,33 +8,4 @@ return {
       ]]
     end,
   },
-
-  {
-    "nvim-lua/plenary.nvim",
-    config = function()
-      local emoji_map = {
-        ["🐧"] = "🐧",
-        ["😄"] = "😄",
-        ["❤️"] = "❤️",
-      }
-
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
-        callback = function()
-          local bufnr = vim.api.nvim_get_current_buf()
-          for k, v in pairs(emoji_map) do
-            vim.api.nvim_buf_set_lines(
-              bufnr,
-              0,
-              -1,
-              false,
-              vim.tbl_map(function(line)
-                return line:gsub(k, v)
-              end, vim.api.nvim_buf_get_lines(bufnr, 0, -1, false))
-            )
-          end
-        end,
-      })
-    end,
-  },
 }
