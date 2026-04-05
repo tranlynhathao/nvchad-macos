0cd30b2b7a81411fb31d27b0b8cde4f8ca5f277f
tranlynhathao <haotran04022005@gmail.com>
2026-04-05 21:53:29 +0700

    fix nvimtree

--- lua/plugins/spec/dropbar.lua
+++ lua/plugins/spec/dropbar.lua
@@ -5,4 +5,24 @@ return {
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
   },
+  config = function()
+    local configs = require "dropbar.configs"
+
+    local default_enable = configs.opts.bar.enable
+    configs.opts.bar.enable = function(buf, win, info)
+      buf = vim._resolve_bufnr(buf)
+      if vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].filetype == "NvimTree" then
+        return false
+      end
+
+      return default_enable(buf, win, info)
+    end
+
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      local buf = vim.api.nvim_win_get_buf(win)
+      if vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].filetype == "NvimTree" then
+        vim.wo[win].winbar = ""
+      end
+    end
+  end,
 }
